c574a76fc9e94f500228be4b04b0cd5f03e5005e
Giovanna Praieiro Pavani <gigipraieiro@gmail.com>
2026-09-08 14:11:07 -0300

    fix: bug01 - rejeitar duracao menor ou igual a zero

--- src/main/java/br/com/fiap/streamfiap/model/Conteudo.java
+++ src/main/java/br/com/fiap/streamfiap/model/Conteudo.java
@@ -22,12 +22,16 @@ public abstract class Conteudo {
     }
 
     protected Conteudo(String titulo, String categoria, int duracaoMinutos, int classificacaoEtaria, boolean disponivel) {
-        this.titulo = titulo;
-        this.categoria = categoria;
-        this.duracaoMinutos = duracaoMinutos;
-        this.classificacaoEtaria = classificacaoEtaria;
-        this.disponivel = disponivel;
+    if (duracaoMinutos <= 0) {
+        throw new IllegalArgumentException("A duração deve ser maior que zero.");
     }
+
+    this.titulo = titulo;
+    this.categoria = categoria;
+    this.duracaoMinutos = duracaoMinutos;
+    this.classificacaoEtaria = classificacaoEtaria;
+    this.disponivel = disponivel;
+}
 
     public double calcularPrecoAluguel() {
         return 9.90;
