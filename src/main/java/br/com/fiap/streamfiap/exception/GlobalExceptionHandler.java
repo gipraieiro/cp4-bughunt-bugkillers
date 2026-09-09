e6ab18016d1d202adc674c2837e722c1b5e63c81
Marie <marieduarda.1908@gmail.com>
2026-09-08 22:37:36 -0300

    fix: bug09 adiciona tratamento para ClassificacaoIndicativaException

--- src/main/java/br/com/fiap/streamfiap/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/fiap/streamfiap/exception/GlobalExceptionHandler.java
@@ -1,27 +1,44 @@
 package br.com.fiap.streamfiap.exception;
-
+ 
 import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+ 
 import java.util.Map;
-
+ 
 @RestControllerAdvice
+
 public class GlobalExceptionHandler {
+ 
+    // Bug 09: Tratamento da exceção de classificação indicativa
 
-    @ExceptionHandler(ConteudoNaoEncontradoException.class)
-    public ResponseEntity<Map<String, String>> handleConteudoNaoEncontrado(ConteudoNaoEncontradoException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("erro", e.getMessage()));
+    @ExceptionHandler(ClassificacaoIndicativaException.class)
+
+    public ResponseEntity<Map<String, String>> handleClassificacaoIndicativa(ClassificacaoIndicativaException ex) {
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+
+                .body(Map.of("erro", ex.getMessage()));
+
+    }
+ 
+    // Bug 10: Retorno estruturado de erros genéricos de aluguel
+
+    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
+
+    public ResponseEntity<Map<String, String>> handleErrosAluguel(RuntimeException ex) {
+
+        // Clean Code 05: Padronização na resposta dos erros da API
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+
+                .body(Map.of("erro", ex.getMessage()));
+
     }
 
-    @ExceptionHandler(CreditosInsuficientesException.class)
-    public ResponseEntity<Map<String, String>> handleCreditosInsuficientes(CreditosInsuficientesException e) {
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(Map.of("erro", e.getMessage()));
-    }
-
-    @ExceptionHandler(ConteudoIndisponivelException.class)
-    public ResponseEntity<Map<String, String>> handleConteudoIndisponivel(ConteudoIndisponivelException e) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("erro", e.getMessage()));
-    }
 }
+ 
